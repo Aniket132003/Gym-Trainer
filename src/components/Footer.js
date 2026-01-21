d897dd7932c6034f182747d0fb80d27b4881a1da
aniket_agrawal <124797476+Aniket132003@users.noreply.github.com>
2026-01-21 21:03:42 +0530

    Update footer text to reflect new author

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => (
     <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
       <img src={Logo} alt="logo" style={{ width: '200px', height: '41px' }} />
     </Stack>
-    <Typography variant="h4" sx={{ fontSize: { lg: '23px', xs: '17px' } }} mt="20px" textAlign="center" pb="40px">Made by - Aditya Agrawal</Typography>
+    <Typography variant="h4" sx={{ fontSize: { lg: '23px', xs: '17px' } }} mt="20px" textAlign="center" pb="40px">Made by - Aniket Agrawal</Typography>
   </Box>
 );
 
