36fa3cafe0e4905585fb9e080e5092920460116d
aniket_agrawal <124797476+Aniket132003@users.noreply.github.com>
2026-01-21 20:49:56 +0530

    Update Exercises.js

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -24,7 +24,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
     };
 
     fetchExercisesData();
-  }, [setExercises]);
+  }, [bodyPart]);
 
   // Pagination
   const indexOfLastExercise = currentPage * exercisesPerPage;
